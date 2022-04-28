3d026f9900a2806fe33725d1c773443291b5cb8e
Megumi Kimura <kimuramegumi@gmail.com>
2022-04-28 08:54:54 +0200

    add central point

--- js/createHtml.js
+++ js/createHtml.js
@@ -3,9 +3,10 @@ const worldClock = document.querySelector(".worldClock");
 export function createHtml(city) {
   worldClock.innerHTML += `<div class="container">
   <div class="clock" id="${city.city}">
-    <div class="hour" id="hour-${city.city}"></div>
-    <div class="minute" id="min-${city.city}"></div>
-    <div class="second" id="sec-${city.city}"></div>
+  <div class="hour" id="hour-${city.city}"></div>
+  <div class="minute" id="min-${city.city}"></div>
+  <div class="second" id="sec-${city.city}"></div>
+  <div class="center"></div>
   </div>
   <h2>${city.city}</h2>
 </div>`;
